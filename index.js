f2654c51d80638582f225d24f5ec152993cda0ce
loshvitalik <Banyaru1603>
2019-01-21 23:25:18 +0500

    Working test solution (without final text)

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ document.getElementById("submitButton").addEventListener("click", checkVerificat
 
 function checkVerificationCode() {
   let resultMessage = document.getElementById("resultMessage");
-  resultMessage.innerText = "";
+  resultMessage.innerText = "Проверка...";
+  let inputText = document.getElementById("verificationCode").value.toLowerCase();
   setTimeout(() => {
-    if (document.getElementById("verificationCode").value === "255")
-      resultMessage.innerText = "Код правильный!";
+    if (inputText.indexOf("ртф") > -1 && inputText.indexOf("чемпион") > -1)
+      //resultMessage.innerHTML = "Следующая страница: 3. &nbsp; Номер предмета: 15.";
+      resultMessage.innerText = "Код верный!";
     else
-      resultMessage.innerText = "Не-а, не угадал";
-  }, 100);
+      resultMessage.innerText = "Этот код не подходит";
+  }, 1000);
 
 }
