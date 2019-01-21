de200f86f83dcf175593e9ad7ea9e5d35853bcd1
loshvitalik <Banyaru1603>
2019-01-21 19:55:40 +0500

    Playing with colors

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 document.getElementById("submitButton").addEventListener("click", checkVerificationCode);
 
 function checkVerificationCode() {
-  if (document.getElementById("verificationCode").value === "255")
-    document.getElementById("resultMessage").innerText = "Код правильный!";
+  let resultMessage = document.getElementById("resultMessage");
+  resultMessage.innerText = "";
+  setTimeout(100, () => {
+    if (document.getElementById("verificationCode").value === "255")
+      resultMessage.innerText = "Код правильный!";
+    else
+      resultMessage.innerText = "Не-а, не угадал";
+  });
+
 }
