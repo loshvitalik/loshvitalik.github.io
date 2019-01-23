51c9b0db4e45b71896c6fbcf0a058b06d2f88925
loshvitalik <Banyaru1603>
2019-01-24 01:19:55 +0500

    Final result with number tests

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ function checkVerificationCode() {
   let inputText = document.getElementById("verificationCode").value.toLowerCase();
   setTimeout(() => {
     if (inputText.indexOf("ртф") > -1 && inputText.indexOf("чемпион") > -1)
-      //resultMessage.innerHTML = "Следующая страница: 3. &nbsp; Номер предмета: 15.";
-      resultMessage.innerText = "Код верный!";
+    resultMessage.innerHTML = "Уровень \"Сессия закрыта\". &nbsp; Объекты №8.1 и 8.2.";
     else
       resultMessage.innerText = "Этот код не подходит";
   }, 1000);
