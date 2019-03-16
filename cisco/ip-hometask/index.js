2e72bedeff72bcad98bc73c4c5944f2b859e06c2
loshvitalik <ban-ya-ru@ya.ru>
2019-03-17 03:30:04 +0500

    Cisco ip-homerask release 1.0

---
+++ cisco/ip-hometask/index.js
@@ -0,0 +1,5 @@
+document.getElementById("donateButton").addEventListener("click", showDonateOptions);
+
+function showDonateOptions() {
+  alert("Сбербанк: +79829155961 \nИли приносите еду по адресу: г. Берёзовский, ул. Театральная, 2/1")
+}
